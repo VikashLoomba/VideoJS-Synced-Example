a8119218bef67e8dd67de2abf4682cd8014f2284
Vikash Loomba <loomba.vikash@gmail.com>
2016-06-12 15:30:28 -0700

    changed functions

--- video.js
+++ video.js
@@ -19,8 +19,8 @@ function playVideo() {
 	myPlayers[1].play();
 }
 function pauseVideo() {
-	sync();
 	myPlayers[1].pause();
+	sync();
 }
 
 myPlayers[0].on('play', function(){
@@ -29,8 +29,27 @@ myPlayers[0].on('play', function(){
 myPlayers[0].on('pause', function(){
 	pauseVideo();
 });
-myPlayers[0].on('seeked', function(){
+myPlayers[0].on('seeking', function(){
 	sync();
+});
+myPlayers[0].on('error',  function (e) {
+   switch (e.target.error.code) {
+     case e.target.error.MEDIA_ERR_ABORTED:
+       alert('You aborted the video playback.');
+       break;
+     case e.target.error.MEDIA_ERR_NETWORK:
+       alert('A network error caused the video download to fail part-way.');
+       break;
+     case e.target.error.MEDIA_ERR_DECODE:
+       alert('The video playback was aborted due to a corruption problem or because the video used features your browser did not support.');
+       break;
+     case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+       alert('The video could not be loaded, either because the server or network failed or because the format is not supported.');
+       break;
+     default:
+       alert('An unknown error occurred.');
+       break;
+   }
 });
 
 
